b64614c3517a9e550241362627318a822b4fc3aa
François Poirotte <clicky@erebot.net>
2014-12-21 11:48:14 +0100

    Add ecdh-sha2-nistp384 & ecdh-sha2-nistp521.
    Those got somehow left out when support for ecdh-sha2-nistp256 was added.

---
+++ src/Messages/KEX/ECDH/INIT/NISTp384.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+* This file is part of pssht.
+*
+* (c) François Poirotte <clicky@erebot.net>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace fpoirotte\Pssht\Messages\KEX\ECDH\INIT;
+
+/**
+ * SSH_MSG_KEX_ECDH_INIT message (RFC 5656).
+ */
+class NISTp384 extends \fpoirotte\Pssht\Messages\KEX\ECDH\INIT
+{
+    public static function getCurve()
+    {
+        return \fpoirotte\Pssht\ECC\Curve::getCurve('nistp384');
+    }
+}
