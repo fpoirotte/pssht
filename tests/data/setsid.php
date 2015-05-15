ec8a45b3092bb2fb89ce4b4ee2dcd86edfd988be
François Poirotte <clicky@erebot.net>
2015-05-15 13:40:48 +0200

    Exit with an error when posix_setsid() fails.
    Should make it easier to debug when tests fail due to underlying issues.

--- tests/data/setsid.php
+++ tests/data/setsid.php
@@ -1,6 +1,9 @@
 <?php
 
-posix_setsid();
+if (posix_setsid() === -1) {
+    fprintf(STDERR, "Could not become session leader!\n");
+    exit(255);
+}
 
 $args = array();
 foreach ($_SERVER['argv'] as $index => $value) {
