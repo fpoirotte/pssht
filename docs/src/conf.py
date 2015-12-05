63db6836069bf3362f047784f2235475f8f938ca
François Poirotte <clicky@erebot.net>
2015-12-05 23:43:24 +0100

    Update the RTD configuration.

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 import glob
 import shutil
 from datetime import datetime
@@ -20,17 +21,22 @@ def prepare(globs, locs):
                 ).stdout.read().strip()
     doxygen = Popen('which doxygen 2> %s' % os.devnull, shell=True, stdout=PIPE
                 ).stdout.read().strip()
+    pybabel = Popen('which pybabel 2> %s' % os.devnull, shell=True, stdout=PIPE
+                ).stdout.read().strip()
 
-    environment = os.path.basename(root)
-    pybabel = os.path.join(root, '..', '..', 'envs',
-                           environment, 'bin', 'pybabel')
-
-    print "git version:"
+    print "git version:",
+    sys.stdout.flush()
     call([git, '--version'])
-    print "doxygen version:"
+
+    print "doxygen version:",
+    sys.stdout.flush()
     call([doxygen, '--version'])
-    print "pybabel version:"
+
+    print "pybabel version:",
+    sys.stdout.flush()
     call([pybabel, '--version'])
+
+    print "environment:", repr(os.environ)
 
     print "Running from %s..." % (root, )
     os.chdir(root)
@@ -38,16 +44,19 @@ def prepare(globs, locs):
     # Figure several configuration values from git.
     origin = Popen([git, 'config', '--local', 'remote.origin.url'],
                    stdout=PIPE).stdout.read().strip()
-    git_tag = Popen(['git', 'describe', '--tags', '--exact', '--first-parent'],
+    git_tag = Popen([git, 'describe', '--tags', '--exact', '--first-parent'],
                     stdout=PIPE).communicate()[0].strip()
-    project = origin.rpartition('/')[2]
-    if project.endswith('.git'):
-        project = project[:-4]
-    locs['project'] = project
+    git_hash = Popen([git, 'rev-parse', 'HEAD'],
+                    stdout=PIPE).communicate()[0].strip()
+    gh_project = ('/'.join(origin.split('/')[-2:])).rpartition(':')[2]
+    if gh_project.endswith('.git'):
+        gh_project = gh_project[:-4]
+    project = gh_project.rpartition('/')[2]
+    os.environ['SPHINX_PROJECT'] = project
     if git_tag:
-        locs['version'] = locs['release'] = git_tag
+        os.environ['SPHINX_VERSION'] = os.environ['SPHINX_RELEASE'] = git_tag
     else:
-        locs['version'] = locs['release'] = 'latest'
+        os.environ['SPHINX_VERSION'] = os.environ['SPHINX_RELEASE'] = 'latest'
         locs['tags'].add('devel')
 
     # Clone or update dependencies
@@ -61,7 +70,7 @@ def prepare(globs, locs):
             os.makedirs(path)
             print "Cloning %s into %s..." % (repository, path)
             call([git, 'clone', repository, path])
-        else:
+        elif os.path.isdir(os.path.join(path, '.git')):
             os.chdir(path)
             print "Updating clone of %s in %s..." % (repository, path)
             call([git, 'checkout', 'master'])
@@ -72,8 +81,8 @@ def prepare(globs, locs):
 
     # Run doxygen
     call([doxygen, os.path.join(root, 'Doxyfile')], env={
-        'COMPONENT_NAME': locs['project'],
-        'COMPONENT_VERSION': locs['version'],
+        'COMPONENT_NAME': os.environ['SPHINX_PROJECT'],
+        'COMPONENT_VERSION': os.environ['SPHINX_VERSION'],
         'COMPONENT_BRIEF': composer.get('description', ''),
     })
 
@@ -108,6 +117,15 @@ def prepare(globs, locs):
     if 'locale_dirs' not in locs:
         locs['locale_dirs'] = []
     locs['locale_dirs'].insert(0, os.path.join(root, 'docs', 'i18n'))
+    if 'rst_prolog' not in locs:
+        locs['rst_prolog'] = ''
+    locs['rst_prolog'] += '\n    .. _`this_commit`: https://github.com/%s/commit/%s\n' % (
+        gh_project,
+        git_hash,
+    )
+
+    globs['RTD_NEW_THEME'] = False
+    globs['RTD_OLD_THEME'] = False
 
 
 prepare(globals(), locals())
